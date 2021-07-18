588b1fe1ed69a043d9e466f9063438a7dac96d92
Matteo34 <67874173+Matteo34@users.noreply.github.com>
2021-07-18 11:27:39 +0200

    aggiunta seconda funzione hash

---
+++ Funzioni Hash/lib/hash_function.h
@@ -0,0 +1,5 @@
+
+
+unsigned long hash_djb2(unsigned char *str);
+
+ unsigned long  hash_fnv_1a(const char* key);
--- Funzioni Hash/testHash.c
+++ Funzioni Hash/testHash.c
@@ -2,17 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "lib/djb2.h"
+#include "lib/hash_function.h"
 
 int main(int argc, char* argv[]){
     if( argc == 0){ printf("Inserisci le parole da testare"); return 0;}
 
-    int i;
+    unsigned long a,i, b;
+
     printf("Quanto vale argc : %d \n", argc); //controllo
     for(i=2 ; i<argc; i++){
-        printf("Per la prima parola %s il risultato è : %lu \n", argv[i], hash_djb2(argv[i]));
+        a = hash_djb2(argv[i]);
+        b= hash_fnv_1a(argv[i]);
+        printf("Per la prima parola %s il risultato è : %lu djb2  %lu fnv1a \n", argv[i], a, b);
+
+        printf("valori delle celle scelte : %ld \n", (a*i + b) % 53 );    
     }
    
     return 1;    
     
 }
+ 
